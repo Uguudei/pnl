3fa1178b4fbf44482bfae434b8254db6c37c4eae
Uguudei <1560166+Uguudei@users.noreply.github.com>
2023-04-20 09:18:43 +0000

    fix logic

--- src/components/TheCalculator.vue
+++ src/components/TheCalculator.vue
@@ -4,8 +4,8 @@ import { ref } from 'vue';
 function getPriceDecimals(price) {
   // Returns decimal count
   let base_price = Math.trunc(Math.abs(price));
-  let decimals = base_price === 0 ? 0 : String(base_price).length;
-  let fractional = 4 - (decimals >= 4 ? decimals : 4);
+  let decimals = base_price === 0 ? 0 : base_price.toString().length;
+  let fractional = 4 - (decimals >= 4 ? 4 : decimals);
   return fractional;
 }
 
@@ -35,6 +35,9 @@ function getTradeRating(r) {
 
 function calcPnl(position, entry, sl, tp, size = 1000, leverege = 10) {
   // Calculate profit & loss
+  // Print config
+  let decimals = getPriceDecimals(entry);
+
   let sign = { 'long': 1, 'short': -1 }
   let reward = (tp / entry - 1) * leverege * sign[position]
   let risk = (sl / entry - 1) * leverege * sign[position]
@@ -42,48 +45,28 @@ function calcPnl(position, entry, sl, tp, size = 1000, leverege = 10) {
   let profit = reward * size
   let loss = risk * size
 
-
   let liquidation = entry - entry / leverege * sign[position] * 0.9 * 0.98
   let breakeven = 1 / (1 + r)
   let riskLevel = getRiskLevel(risk)
   let tradeRating = getTradeRating(r)
 
-  // Print config
-  let decimals = getPriceDecimals(entry);
-  // let align = 11 + (decimals === 0 ? 1 : decimals) - 2;
+  reward = (reward * 100).toFixed(1) + '%'
+  risk = (risk * 100).toFixed(1) + '%'
+  r = Math.round(r, 0.1).toFixed(1)
+  profit = Math.round(profit, 1).toFixed(1) + '$'
+  loss = Math.round(loss, 1).toFixed(1) + '$'
+
+  riskLevel = riskLevel.charAt(0).toUpperCase() + riskLevel.slice(1)
+  tradeRating = tradeRating.charAt(0).toUpperCase() + tradeRating.slice(1)
+  liquidation = liquidation.toFixed(decimals) + '$'
+  breakeven = (breakeven * 100).toFixed(1) + '%'
+
   return { reward, risk, profit, loss, r, liquidation, breakeven, riskLevel, tradeRating, decimals }
 }
 
-function long(entry, sl, tp, size = 1000, leverege = 10) {
+function calcLong() {
   // Calculate longing risk and reward
-  calcPnl('long', entry, sl, tp, size, leverege);
-}
-
-function short(entry, sl, tp, size = 1000, leverege = 10) {
-  // Calculate shorting risk and reward
-  calcPnl('short', entry, sl, tp, size, leverege);
-}
-
-const entry = ref(1000)
-const stopLoss = ref(950)
-const takeProfit = ref(1200)
-const size = ref(1000)
-const leverage = ref(10)
-
-const reward = ref(0)
-const risk = ref(0)
-const profit = ref(0)
-const loss = ref(0)
-const rrr = ref(0)
-
-const riskLevel = ref('')
-const tradeRating = ref('')
-const liquidation = ref(0)
-const breakeven = ref(0)
-
-function callPnl() {
   let result = calcPnl('long', entry.value, stopLoss.value, takeProfit.value, size.value, leverage.value)
-
   reward.value = result.reward
   risk.value = result.risk
   rrr.value = result.r
@@ -96,16 +79,38 @@ function callPnl() {
   breakeven.value = result.breakeven
 }
 
+// function calcShort() {
+//   // Calculate shorting risk and reward
+//   let result = calcPnl('short', entry.value, stopLoss.value, takeProfit.value, size.value, leverage.value)
+// }
+
+const entry = ref(10.0)
+const stopLoss = ref(9.5)
+const takeProfit = ref(12.0)
+const size = ref(1000)
+const leverage = ref(10)
+
+const reward = ref()
+const risk = ref()
+const profit = ref()
+const loss = ref()
+const rrr = ref()
+
+const riskLevel = ref()
+const tradeRating = ref()
+const liquidation = ref()
+const breakeven = ref()
+
 </script>
 
 <template>
   <div>
-    <input v-model="entry" placeholder="Entry">Entry<br>
-    <input v-model="stopLoss" placeholder="Stop loss">SL<br>
-    <input v-model="takeProfit" placeholder="Take profit">TP<br>
-    <input v-model="size" placeholder="Size">Size<br>
-    <input v-model="leverage" placeholder="Leverage">X<br>
-    <button @click='callPnl'>Calculate</button>
+    <input v-model='entry' placeholder='Entry'>Entry<br>
+    <input v-model='stopLoss' placeholder='Stop loss'>SL<br>
+    <input v-model='takeProfit' placeholder='Take profit'>TP<br>
+    <input v-model='size' placeholder='Size'>Size<br>
+    <input v-model='leverage' placeholder='Leverage'>X<br>
+    <button @click='calcLong'>Calculate</button>
     <br>
     <!-- <h2>{{ callPnl() }}</h2> -->
     <p>Reward: {{ reward }}</p>
@@ -118,8 +123,6 @@ function callPnl() {
     <p>Rating: {{ tradeRating }}</p>
     <p>Liquidation: {{ liquidation }}</p>
     <p>Breakeven: {{ breakeven }}</p>
-
-
   </div>
 </template>
 
